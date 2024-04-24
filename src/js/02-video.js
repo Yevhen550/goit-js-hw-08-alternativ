3761399072902b5ab461bf93c542258a56df1c82
Yevhen Liaskivskyi <benatanlev@icloud.com>
2024-04-24 11:09:19 +0300

    some

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,8 @@
+const video = document.querySelector('#vimeo-player');
 
+// console.log(video);
+// function timeSet(current) {
+//   localStorage.getItem('time-video', current);
+// }
+// console.log(timeSet());
+console.log(localStorage.getItem('sync_active'));
