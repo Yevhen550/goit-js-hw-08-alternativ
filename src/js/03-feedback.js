a0f14734f2de0132621ce3ac77f115e875aa186a
Yevhen Liaskivskyi <benatanlev@icloud.com>
2024-04-26 14:29:47 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,11 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const LOCAL_KEY = 'feedback-form-state';
+
+form.addEventListener('input', inputValue);
+
+function inputValue(ev) {
+  const targetValue = ev.target.value;
+  localStorage.setItem(LOCAL_KEY, targetValue);
+}
