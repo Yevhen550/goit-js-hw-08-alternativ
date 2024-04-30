752d764d5cf35d71c9c743c778bf9a0d7b24384f
Yevhen Liaskivskyi <benatanlev@icloud.com>
2024-04-30 19:00:40 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,11 +1,32 @@
 import throttle from 'lodash.throttle';
 
 const form = document.querySelector('.feedback-form');
+const textEmail = document.querySelector('[name="email"]');
+const textMessage = document.querySelector('[name="message"]');
+
 const LOCAL_KEY = 'feedback-form-state';
 
-form.addEventListener('input', inputValue);
+let storageObj = {};
+
+form.addEventListener('input', throttle(inputValue, 500));
+form.addEventListener('submit', onFormSubmit);
 
 function inputValue(ev) {
-  const targetValue = ev.target.value;
-  localStorage.setItem(LOCAL_KEY, targetValue);
+  const nameTarget = ev.target.name;
+  const valueTarget = ev.target.value;
+
+  if (nameTarget === 'email') {
+    storageObj.email = valueTarget;
+  } else if (nameTarget === 'message') {
+    storageObj.message = valueTarget;
+  }
+
+  localStorage.setItem(LOCAL_KEY, JSON.stringify(storageObj));
+}
+
+function onFormSubmit(ev) {
+  ev.preventDefault();
+  console.log(storageObj);
+  localStorage.removeItem(LOCAL_KEY);
+  form.reset();
 }
